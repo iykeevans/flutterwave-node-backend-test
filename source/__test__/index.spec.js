2d995b4433b0b5a6dbcc5a0d7faa1e2ac97fd629
Ezeani Ikenna <ikenna.ezeani@scelloo.com>
2021-01-28 16:58:54 +0100

    feat: implement get profile route

--- source/__test__/index.spec.js
+++ source/__test__/index.spec.js
@@ -1,10 +1,22 @@
 const supertest = require("supertest");
 const app = require("../app");
 
-describe("Testing simple rule-validation API index route", () => {
-  it("Should return hello world", async () => {
+describe("Testing simple rule-validation API '/' route", () => {
+  it("Should return my profile object", async () => {
+    const expectedData = {
+      message: "My Rule-Validation API",
+      status: "success",
+      data: {
+        name: "Ezeani Ikenna",
+        github: "@iykeevans",
+        email: "elochi238@gmail.com",
+        mobile: "07053052215",
+        twitter: "@iykeevan",
+      },
+    };
+
     const response = await supertest(app).get("/");
 
-    expect(response.text).toBe("Hello World");
+    expect(response.body).toEqual(expectedData);
   });
 });
